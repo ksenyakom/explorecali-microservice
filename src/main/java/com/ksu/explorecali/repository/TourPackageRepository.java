21dba8bb92f7cd9a360f9833aa6a73d79cd8013a
Kseniya_Oznobishina <Kseniya_Oznobishina@epam.com>
2023-02-17 10:58:05 +0300

    Initial commit

---
+++ src/main/java/com/ksu/explorecali/repository/TourPackageRepository.java
@@ -0,0 +1,50 @@
+package com.ksu.explorecali.repository;
+
+import com.ksu.explorecali.domain.Tour;
+import com.ksu.explorecali.domain.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.Optional;
+
+/**
+ * Tour Package Repository Interface
+ * <p>
+ * Created by Mary Ellen Bowman
+ */
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
+public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+
+    Optional<TourPackage> findByName(String name);
+
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S entity);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage entity);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAllById(Iterable<? extends String> strings);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> entities);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+}
+
---
+++ src/test/java/com/ksu/explorecali/ExplorecaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksu.explorecali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorecaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
